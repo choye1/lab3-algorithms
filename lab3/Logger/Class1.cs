d5bd25cbd2959c01cb904d6a2dfff592bdbc9bb9
choye1 <makcim233237@gmail.com>
2024-11-01 22:40:14 +0500

    теперь даже одыкватно

--- lab3/Logger/Class1.cs
+++ lab3/Logger/Class1.cs
@@ -6,9 +6,13 @@
         public readonly string path = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()).ToString()).ToString()).ToString() + "Log.txt";
         public void Write(string msg) 
         {
-            File.AppendAllText(path, msg + "\n");
+            File.AppendAllText(path, msg);
         }
 
+        public void WriteLine(string msg)
+        {
+            File.AppendAllText(path, msg + "\n");
+        }
         public string[] Read()
         {
             return File.ReadAllLines(path);
