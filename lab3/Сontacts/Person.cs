a399eda614b522fc18330ebb2d0d6b94d7f073bb
Aleksey Salamatin <asalamatin640@gmail.com>
2024-11-09 22:31:25 +0500

    написал список контактов

---
+++ lab3/Сontacts/Person.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Сontacts
+{
+    public class Person
+    {
+        private string name;
+        private string surname;
+        private int phoneNumber;
+
+        public Person(string name, string surname, int phoneNumber)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.phoneNumber = phoneNumber;
+        }
+        public Person(string name, int phoneNumber)
+        {
+            this.phoneNumber = phoneNumber;
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public string GetSurname()
+        {
+            return surname;
+        }
+        public int GetPhoneNumber()
+        {
+            return phoneNumber;
+        }
+    }
+}
