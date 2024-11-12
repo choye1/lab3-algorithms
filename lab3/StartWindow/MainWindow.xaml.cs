1bedd562a99c112151cae591736d7a99aa049802
DaniilPrikhoddko <d1912dd@gmail.com>
2024-11-12 22:00:53 +0500

    связи

---
+++ lab3/StartWindow/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BankQueueSystem;
+using lab3;
+using ListDynamicStructures;
+using Loggers;
+using OrganizationApp;
+using part4;
+using PersonContactsApp;
+using DynamicStructuresEntities;
+using RPNEntities;
+using StackHandlers;
+using TextProcessorApp;
+
+namespace StartWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
